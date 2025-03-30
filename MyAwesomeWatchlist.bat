53d7e82c085ef88b0ca064a4996f1e30d09f1eec
Alexander Marini <svenskost6@gmail.com>
2025-03-30 17:06:56 +0200

    Delete MyAwesomeWatchlist.bat

--- MyAwesomeWatchlist.bat
+++
@@ -1,4 +0,0 @@
-cd "C:\Users\Alexander\projects\my-awesome-watchlist"
-start /min wt -d client cmd /k "npm run dev" ; split-pane -d server --title "My Awesome Watchlist" cmd /k "npm run dev"
-start msedge http://localhost:3000
-exit
